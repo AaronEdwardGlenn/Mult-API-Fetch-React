f0481c87c0febaaa127d97b38fb6d0fbc935f41c
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-01 18:48:25 -0800

    added quotefetcheer and mock files for testing

---
+++ src/components/__mock__/quotesAPI.js
@@ -0,0 +1,16 @@
+export const getRonQuote = () => {
+  return Promise.resolve({
+    text: 'Cool', 
+    name: 'Quote', 
+    img: 'asdf/asdf/cool.png'
+  });
+};
+
+export const getFuturagamQuote = () => {
+  return Promise.resolve({
+    text: 'Cooasdfl', 
+    name: 'Quasdfote', 
+    img: 'asddf/adsdf/codol.png'
+  });
+};
+  
--- src/services/quotesAPI.js
+++ src/services/quotesAPI.js
@@ -1,6 +1,10 @@
 export const getRonQuote = () => {
-  return;
-  fetch();
+  return fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes/1')
+    .then(res => res.json())
+    .then(([text]) => ({
+      text,
+      img: 'http://placekitten.com/200/300'
+    }));
 }; 
 
 export const getFuturamaQuote = () => {

a5f4dbdea717be084f0bc0bda473d5f6b7dfdc77
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-01 19:00:59 -0800

    added tests for quotefetcher

--- src/components/containers/QuoteFetcher.test.js
+++ src/components/containers/QuoteFetcher.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import QuoteFetcher from './QuoteFetcher';
 
+jest.mock('../services/quotesAPI.js'); 
+
 describe('quote fetch container', () => {
   it('changes quote providers', () => {
     const wrapper = shallow(<QuoteFetcher />);
@@ -14,6 +16,19 @@ describe('quote fetch container', () => {
     });
     expect(wrapper.state('quoteProvider')).toEqual('futurama');
   });
+  it('fetches from the correct API based on quoteProvider func', () => {
+    const wrapper = shallow(<QuoteFetcher />); 
+    const quoteFetcher = wrapper.instance(); 
+    
+    wrapper.setState({ quoteProvier: 'ron' })
+
+    quoteFetcher.fetch(); 
+  });
+  expect(wrapper.state('quote').toEqual({
+      text: 'Cool', 
+      name: 'Quote', 
+      img: 'asdf/asdf/cool.png'
+    });
 });
 
 
